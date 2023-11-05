be232ac029642f783c0b86444832701959fbca1d
duydl <duy.dole.00ece@gmail.com>
2023-11-06 02:09:46 +0900

    fix: use flipflop to detect edge

--- hdl/pulse.sv
+++ hdl/pulse.sv
@@ -5,18 +5,24 @@ module sc_pulse(
     output logic bit_stream    // Output bit stream
 );
     logic [15:0] count = 16'b0;  // Counter to control the output bit duration
+    logic pulse_edge;           // Detect positive edge of pulse_input
 
     always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
             bit_stream <= 1'b0;  // Reset bit_stream
             count <= 16'b0;      // Reset the counter
+            pulse_edge <= 1'b0;  // Reset the positive edge detector
         end else begin
-            if (pulse_input && count == 100) begin
-                bit_stream <= 1'b1;  // Set bit_stream to 1
-                count <= 0;
-                // if (count == 100) begin
-                //     count <= 0;  // Increment the counter
-                // end
+            // Detect positive edge of pulse_input
+            if (pulse_input && !pulse_edge) begin
+                pulse_edge <= 1'b1;
+            end else begin
+                pulse_edge <= 1'b0;
+            end
+
+            if (pulse_edge) begin
+                bit_stream <= 1'b1;  // Set bit_stream to 1 on positive edge
+                count <= 0;           // Reset the counter
             end else if (count < 100) begin
                 bit_stream <= 1'b1; 
                 count <= count + 1;
